27da8784c4d043a585e216aa215f0a7d4c9d3455
kuwa2005 <kuwahar@gmail.com>
2026-06-30 06:52:22 +0000

    chore: メンテナンスモード解除

---
+++ lib/maintenance.ts
@@ -0,0 +1,7 @@
+/** メンテナンス告知バナー。false にすると非表示（機能制限は行わない）。 */
+export const MAINTENANCE_MODE = false;
+
+export const MAINTENANCE_MESSAGE = "只今メンテナンス中です";
+
+export const MAINTENANCE_SUBMESSAGE =
+  "オフラインMP4の出力がおかしい場合があります。";
